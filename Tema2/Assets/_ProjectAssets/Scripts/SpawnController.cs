a64d7d593635e0b081b9a5088a2eef34ba078378
Curcudel Teodor <teocurcudel@gmail.com>
2023-10-21 11:17:51 +0300

    Am adaugat tema 2

---
+++ Tema2/Assets/_ProjectAssets/Scripts/SpawnController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine("Military target");
+    }
+
+    void Update()
+    {
+        
+    }
+}

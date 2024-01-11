592c7b8d849ce255f8c97b14d06442ebb71dcde1
Robert <b_robert2002@yahoo.com>
2024-01-11 19:26:53 +0200

    added the monster that is chasing us and its animations

--- Vr Shooter - v2/Assets/_ProjectAssets/Scripts/MonsterScriptChasing.cs
+++ Vr Shooter - v2/Assets/_ProjectAssets/Scripts/MonsterScriptChasing.cs
@@ -1,25 +1,43 @@
 using UnityEngine;
+using System.Collections;
 
 public class MonsterChasingScript : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
+    public float moveSpeed = 5f;  // Adjust this value to control the speed of the chasing
+    private bool hasStartedMoving = false;
+
+    private Transform playerTransform;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        playerTransform = Camera.main.transform; // Assuming you want to move towards the main camera
     }
 
     // Update is called once per frame
     void Update()
     {
-        // You can add additional behavior here if needed
+        if (playerTransform != null)
+        {
+            // Move towards the player
+            animator.SetTrigger("Run Forward W Root");
+            // animator.SetTrigger("Attack 01");
+            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+
+            // You can add additional behavior here if needed
+        }
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+
+        // Set the trigger named "TakeDamage" on the animator
+        animator.SetTrigger("Take Damage");
 
         // Add any additional behavior upon taking damage (e.g., play a hit animation, show health bar, etc.)
 
@@ -31,6 +49,15 @@ public class MonsterChasingScript : MonoBehaviour
 
     void Die()
     {
+        animator.SetTrigger("Die");
+        StartCoroutine(PauseAndDestroy(2f));
+    }
+
+    IEnumerator PauseAndDestroy(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+
+        // This will be executed after waiting for the specified duration
         Destroy(gameObject);
     }
 
@@ -42,7 +69,7 @@ public class MonsterChasingScript : MonoBehaviour
             Debug.Log("Bullet hit!");
             int damage = 50;
             TakeDamage(damage);
-            Debug.Log("Currect health: " + currentHealth);
+            Debug.Log("Current health: " + currentHealth);
             Destroy(other.gameObject);
         }
     }

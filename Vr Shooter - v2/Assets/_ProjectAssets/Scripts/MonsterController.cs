2ea6be0717ac07b8789eb97630f7048e7a78cebc
Curcudel Teodor <teocurcudel@gmail.com>
2024-01-14 22:39:42 +0200

    Added NavMesh for the enemy. It follows the player but only the 1st frame of the Idle animation is played when he reaches the player.

---
+++ Vr Shooter - v2/Assets/_ProjectAssets/Scripts/MonsterController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterController : MonoBehaviour
+{
+
+    public Transform player; // Reference to the player (Main Camera in this case)
+    public Animator animator;
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        if (player == null)
+        {
+            player = Camera.main.transform;
+        }
+    }
+
+    void Update()
+    {
+        animator.SetBool("isIdle", false);
+        animator.SetTrigger("Run Forward"); // daca scoatem asta apare si animatia de hit, altfel nu mereu
+        navMeshAgent.SetDestination(player.position);
+
+        // Project both positions onto the same plane (ignore the Y component)
+        Vector3 monsterPos = new Vector3(transform.position.x, 0f, transform.position.z);
+        Vector3 playerPos = new Vector3(player.position.x, 0f, player.position.z);
+
+
+        if (Vector3.Distance(monsterPos, playerPos) <= navMeshAgent.stoppingDistance)
+        {
+            Debug.Log("Banana");
+            animator.SetBool("isIdle", true);
+        }
+    }
+}

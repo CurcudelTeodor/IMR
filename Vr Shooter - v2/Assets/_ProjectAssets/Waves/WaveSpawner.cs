8b7866d172e89132b065e7c6d200bbfef905f1fd
Curcudel Teodor <teocurcudel@gmail.com>
2024-02-07 19:05:34 +0200

    Added Text before each wave.

--- Vr Shooter - v2/Assets/_ProjectAssets/Waves/WaveSpawner.cs
+++ Vr Shooter - v2/Assets/_ProjectAssets/Waves/WaveSpawner.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour
 {
     public Wave[] waves;
     public Transform player;
     public Camera playerCamera;
+
+    public TextMeshProUGUI waveText;
+    public Canvas waveCanvas;
+    public float waveTextDuration = 3f;
+    
     private Wave currentWave;
 
     [SerializeField]
@@ -18,6 +25,7 @@ public class WaveSpawner : MonoBehaviour
 
     private void Awake()
     {
+        waveCanvas.enabled = false; // Disable the canvas initially
         StartNextWave();
     }
 
@@ -37,6 +45,7 @@ public class WaveSpawner : MonoBehaviour
     private IEnumerator DelayBeforeNextWave()
     {
         yield return new WaitForSeconds(currentWave.PauseTimeAfterThisWaveEnded);
+
         StartNextWave();
     }
 
@@ -46,17 +55,32 @@ public class WaveSpawner : MonoBehaviour
         {
             currentWave = waves[currentWaveIndex];
             StartCoroutine(SpawnWaveEnemies());
+            UpdateWaveText(currentWaveIndex + 1); // Update wave text with the current wave number
             currentWaveIndex++;
             waveInProgress = true;
+
+            // Enable the canvas at the start of each wave
+            waveCanvas.enabled = true;
         }
         else
         {
             stopSpawning = true;
         }
+        
+    }
+
+    private void UpdateWaveText(int waveNumber)
+    {
+        if (waveText != null)
+        {
+            waveText.text = "Wave " + waveNumber.ToString() + ". Get Ready!!";
+        }
     }
 
     private IEnumerator SpawnWaveEnemies()
     {
+        waveCanvas.enabled = true; // Show the canvas at the start of the wave
+
         int enemiesToSpawn = currentWave.NumberToSpawn;
         int enemiesSpawned = 0;
 
@@ -85,6 +109,14 @@ public class WaveSpawner : MonoBehaviour
         }
 
         waveInProgress = false;
+        StartCoroutine(HideWaveTextAfterDelay());
+
+    }
+
+    private IEnumerator HideWaveTextAfterDelay()
+    {
+        yield return new WaitForSeconds(waveTextDuration);
+        waveCanvas.enabled = false; // Hide the canvas after the specified duration
     }
 
     private bool AllEnemiesDefeated()
